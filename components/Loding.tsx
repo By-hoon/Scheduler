9ad272f145f38519c3b5f193e324a4183b1f9a0b
연훈 배 <sbaru1997@naver.com>
2022-01-11 03:43:14 +0900

    feat: 로딩페이지 구현

---
+++ components/Loding.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const LodingContainer = styled.div``;
+
+const LodingSpan = styled.span``;
+
+const Loding = () => {
+  return (
+    <LodingContainer>
+      <LodingSpan>로딩중</LodingSpan>
+    </LodingContainer>
+  );
+};
+
+export default Loding;
