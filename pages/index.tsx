f3a32bb49b3db62a1a821c8bc8adb8b01e45deb7
연훈 배 <sbaru1997@naver.com>
2022-01-11 03:46:26 +0900

    feat: 메인 페이지 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
+import styled from "styled-components";
+import Auth from "hooks/useAuth";
+import LogInRequest from "components/LogInRequest";
+import Loding from "components/Loding";
+
+const LoggedInContainer = styled.div``;
+const LoggedInSpan = styled.span``;
+
 export default function Home() {
-  return "Home";
+  const userObj = Auth();
+  return (
+    <>
+      {userObj === undefined ? (
+        <Loding />
+      ) : userObj === null ? (
+        <LogInRequest />
+      ) : (
+        <LoggedInContainer>
+          <LoggedInSpan>Welcome {userObj.uName}</LoggedInSpan>
+        </LoggedInContainer>
+      )}
+    </>
+  );
 }
