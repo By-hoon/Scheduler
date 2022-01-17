f7be5c90a824c147e46acc7528c666f0f0a82a53
연훈 배 <sbaru1997@naver.com>
2022-01-17 22:04:33 +0900

    feat: 로그인 페이지 이동 로직 변경
    LoginRequest.tsx => 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import styled from "styled-components";
 import Auth from "hooks/useAuth";
-import LogInRequest from "components/LogInRequest";
 import Loding from "components/Loding";
 import Seo from "components/SEO";
+import Router from "next/router";
 
 export default function Home() {
   const userObj = Auth();
+
+  const moveLogin = () => {
+    Router.push("/login");
+  };
   return (
     <>
       {userObj === undefined ? (
         <Loding />
       ) : userObj === null ? (
-        <LogInRequest />
+        moveLogin()
       ) : (
         <>
           <Seo title="홈 | Scheduler" />
